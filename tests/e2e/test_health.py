11883dc8f1b8ce26a25025853ef24a91317c7f0f
adriangs1996 <adriangonzalezsanchez1996@gmail.com>
2022-01-27 00:05:06 -0500

    Add health endpoint & test for first app endpoint

---
+++ tests/e2e/test_health.py
@@ -0,0 +1,10 @@
+from fastapi.testclient import TestClient
+from fastapi import status
+from main import api
+
+client = TestClient(api)
+
+
+def test_api_is_live():
+    response = client.get("/health")
+    assert response.status_code == status.HTTP_200_OK
