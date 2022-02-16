9b0f4dff16db2b7a9e21c9f145ae5a6ee303ede0
adriangs1996 <adriangonzalezsanchez1996@gmail.com>
2022-02-15 19:27:22 -0500

    Add cors middleware support

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from src.controllers import health, demo, account, user
 from src.config import config
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = ['*']
 
 api = FastAPI(docs_url="/")
+api.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=['*'],
+                   allow_headers=['*'])
 api.include_router(health.router)
 api.include_router(demo.router)
 api.include_router(account.router)
