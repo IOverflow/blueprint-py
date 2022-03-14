d49bd009024f55c6aeb4f7a6768ac93302e26549
adriangs1996 <adriangonzalezsanchez1996@gmail.com>
2022-03-14 18:52:09 -0400

    Upgrade nginx config

--- src/main.py
+++ src/main.py
@@ -5,13 +5,20 @@ from fastapi.middleware.cors import CORSMiddleware
 
 origins = ['*']
 
-api = FastAPI(docs_url="/")
+api = FastAPI(docs_url=None,
+              redoc_url='/api/v1/admin/docs',
+              openapi_url='/api/v1/admin/openapi.json',
+              title="Admin Project API",
+              version="0.1.0",
+              contact={
+                  'name': "NextX Team"
+              })
 api.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=['*'],
                    allow_headers=['*'])
-api.include_router(health.router)
-api.include_router(account.router)
-api.include_router(user.router)
-api.include_router(nomenclature.router)
+api.include_router(health.router, prefix='/api/v1/admin')
+api.include_router(account.router, prefix='/api/v1/admin')
+api.include_router(user.router, prefix='/api/v1/admin')
+api.include_router(nomenclature.router, prefix='/api/v1/admin')
 
 
 @api.on_event("startup")
